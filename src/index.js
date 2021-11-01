2b7b9617fe3ac92d6cc4325a040328b2cf308998
MeowHow <sss.www201011@gmail.com>
2021-11-01 19:42:19 +0800

    1101更新

--- src/index.js
+++ src/index.js
@@ -239,6 +239,14 @@ client.on('message', async (msg) => {
 			if(msg.author.id == '279250173979525121') {
 				msg.channel.send('https://tenor.com/baz4u.gif');
 			}
+		} else if(st.indexOf("恭喜") != -1){
+			//Math.floor(Math.random() * (max - min + 1) + min)
+			let rndInt = Math.floor(Math.random() * (10 - 1 + 1) + 1)
+			if(rndInt >= 5) {
+				msg.channel.send("https://tenor.com/brJVM.gif");
+			} else { 
+				msg.channel.send("https://tenor.com/bzqi5.gif");
+			}
 		}
 		
 		/*else if(st.indexOf("蘭蘭我婆") != -1){
