955cdf8d2adf7c6f0a6475976236f9cf0b65f040
MeowHow <sss.www201011@gmail.com>
2021-11-11 15:09:15 +0800

    1111更新

---
+++ src/Commands/測試用.js
@@ -0,0 +1,18 @@
+/** @format */
+
+const Command = require("../Structures/Command.js");
+
+
+module.exports = new Command({
+	name: "測試用",
+	description: "測試反應用區塊",
+	async run(message, args, client) {
+		try {
+			//反應區
+			
+		} catch (error) {
+			message.channel.send('我似乎該有反應，但貓豪寫錯程式了，不過貓豪還是很棒，uma<3。');
+			console.log(error);
+		}
+	}
+});
