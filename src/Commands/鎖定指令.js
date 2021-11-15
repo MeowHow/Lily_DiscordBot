64495a7008a31d94ab59e8eeee712857cce6b9f0
KaMu <kamu@codegod.com.tw>
2021-11-15 13:09:28 +0800

    remove lock command role limit

--- src/Commands/鎖定指令.js
+++ src/Commands/鎖定指令.js
@@ -8,18 +8,17 @@ module.exports = new Command({
     description: "特定身分，使用鎖定強制讓特定地雷出現",
     async run(message, args, client) {
         try {
-            if (message.member.roles.cache.has(config.EngineerID)) {
-                let getID = message.author.id;
-                //反應區
-                if (config.LockID.indexOf(getID) == -1) {
-                    message.channel.send('鎖定目標!');
-                    config.LockID.push(getID);
-                } else {
-                    message.channel.send("鎖定模式已經啟用")
-                }
-            }else if(message.member.roles.cache.has(config.CthulhuID))
-            {
-                message.channel.send("信徒還想鎖定放導彈阿")
+
+            let getID = message.author.id;
+            //反應區
+            if (config.LockID.indexOf(getID) == -1) {
+                message.channel.send('鎖定目標!');
+                config.LockID.push(getID);
+            } else {
+                message.channel.send("鎖定模式已經啟用")
+            }
+            if (message.author.id==config.XgID) {
+                message.channel.send("茶几還想鎖定啊?，好啦給你鎖")
             }
 
         } catch (error) {
