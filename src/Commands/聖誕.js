c751c7ce05420cf16d1877164bc364d2deec925f
MeowHow <sss.www201011@gmail.com>
2022-01-08 06:47:04 +0800

    0108更新

--- src/Commands/聖誕.js
+++ src/Commands/聖誕.js
@@ -4,11 +4,12 @@ const Command = require("../Structures/Command.js");
 
 module.exports = new Command({
 	name: "聖誕",
-	description: "此地雷有加入至cmdlist使其成為包含詞",
+	description: "此加入至cmdlist使其成為包含詞",
 	async run(message, args, client) {
 		try {
 			//反應區
 			//Math.floor(Math.random() * (max - min + 1) + min)
+			/*
 			let rndInt = Math.floor(Math.random() * (15 - 1 + 1) + 1)
 			if(rndInt <= 5) {
 				message.channel.send("https://tenor.com/bfdfS.gif");
@@ -17,6 +18,12 @@ module.exports = new Command({
 			} else if (rndInt > 10 && rndInt <= 15) { 
 				message.channel.send("https://tenor.com/2tTI.gif");
 			}
+			*/
+			message.channel.send({
+				files: [
+					"./Source/soon.jpg"
+				]
+			});
 		} catch (error) {
 			message.channel.send('我似乎該有反應，但貓豪寫錯程式了，不過貓豪還是很棒，uma<3。');
 			console.log(error);
--- src/Commands/補刀咒式.js
+++ src/Commands/補刀咒式.js
@@ -9,24 +9,34 @@ module.exports = new Command({
 	async run(message, args, client) {
 		try {
 			//反應區
-			let dt = new Date();
-			Nowtime = dt.getTime();
-			if(Nowtime - time > cooling) {
-				message.channel.send('準備施術。');
+			if(message.author.id == config.XgID) {
 				message.channel.send({
 					files: [
-						"./Source/茶几看招.gif"
+						"./Source/不要自爆.gif"
 					]
 				});
-				time = Nowtime;
 			} else {
-				let Milliseconds = (cooling - (Nowtime - time)); // /60000
-				let seconds = Math.floor(Milliseconds/1000); 
-				let minutes = Math.floor(seconds/60); 
-				seconds = seconds - (minutes * 60);  
-				message.channel.send('冷卻中，剩餘時間' + minutes + ':' + seconds);
-				console.log();
+				let dt = new Date();
+				Nowtime = dt.getTime();
+				if(Nowtime - time > cooling) {
+					message.channel.send('準備施術。');
+					message.channel.send({
+						files: [
+							"./Source/茶几看招.gif"
+						]
+					});
+					time = Nowtime;
+				} else {
+					let Milliseconds = (cooling - (Nowtime - time)); // /60000
+					let seconds = Math.floor(Milliseconds/1000); 
+					let minutes = Math.floor(seconds/60); 
+					seconds = seconds - (minutes * 60);  
+					message.channel.send('冷卻中，剩餘時間' + minutes + ':' + seconds);
+					console.log();
+				}
 			}
+
+			
 		} catch (error) {
 			message.channel.send('我似乎該有反應，但貓豪寫錯程式了，不過貓豪還是很棒，uma<3。');
 			console.log(error);
